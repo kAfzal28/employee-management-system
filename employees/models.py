f8febc0c8597df0fdb585fd53033fd0878ec619e
kAfzal28 <iamafzalkhan28@gmail.com>
2026-07-03 02:05:03 +0530

    Initialize EMS project with MySQL, Employee app and Admin

---
+++ employees/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department = models.CharField(max_length=50)
+    joining_date = models.DateField()
