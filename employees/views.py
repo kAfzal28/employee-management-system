619ed982cf53e43afd25a2725044881ff410d091
kAfzal28 <iamafzalkhan28@gmail.com>
2026-07-05 22:44:48 +0530

    Complete Add Employee feature with success messages and navigation

--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
 from .models import Employee
+from django.contrib import messages
 
 
 def home(request):
@@ -10,4 +11,27 @@ def home(request):
     })
 
 def add_employee(request):
-    return render(request,"employees/add_employee.html")
+
+    if request.method == "POST":
+        employee_id = request.POST.get("employee_id")
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        department = request.POST.get("department")
+        joining_date = request.POST.get("joining_date")
+        
+        employee = Employee(
+        employee_id=employee_id,
+        name=name,
+        email=email,
+        department=department,
+        joining_date=joining_date,
+    )
+        
+        employee.save()
+        
+        messages.success(request, "Employee added successfully.")
+        
+        return redirect("add_employee")
+        
+
+    return render(request, "employees/add_employee.html")
